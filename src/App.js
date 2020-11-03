e1c4f47fc38a7ef5fa4ff4a9ff73cecfa7965ca3
Vincent McGargill <vincentmcgargill@gmail.com>
2020-11-02 20:02:28 -0700

    Application is finished. Table is built with ag-grid-community and react-dropdown-select. User can now filter throught the table by department of the employee. Project is finished and ready to deploy to Heroku.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import Select from "react-dropdown-select";
+import Employees from './Employees'
+import 'ag-grid-enterprise';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [gridApi, setGridApi] = useState(null);
+    const [gridColumnApi, setGridColumnApi] = useState(null);
+
+    const [rowData, setRowData] = useState(Employees);
+
+    function onGridReady(params) {
+        setGridApi(params.api);
+        setGridColumnApi(params.columnApi);
+    }
+
+    const options = [
+      {value: "Executive", label: "Executive"},
+      {value: "Human Resources", label: "Human Resources"},
+      {value: "Management", label: "Management"},
+      {value: "IT", label: "IT"},
+      {value: "Sales", label: "Sales"},
+      {value: "Production", label: "Production"},
+      {value: "All", label: "All"}
+    ]
+  
+    const setValues = (values) => {
+      const value = values[0].value
+      if (value === "All") {
+        setRowData(Employees)
+      } else {
+        const filterEmployees = Employees.filter((employee) => {
+          return employee.department == value;
+        });
+        setRowData(filterEmployees)
+      }
+    }
+
+    return (
+        <div className="ag-theme-alpine" style={ { height: 525 } }>
+          <Select options={options} placeholder="Select to filter employees by department" onChange={(value) => setValues(value)}/><br/>
+          <AgGridReact
+              onGridReady={onGridReady}
+              rowData={rowData}>
+              <AgGridColumn field="name"></AgGridColumn>
+              <AgGridColumn field="title"></AgGridColumn>
+              <AgGridColumn field="department"></AgGridColumn>
+              <AgGridColumn field="salary"></AgGridColumn>
+              <AgGridColumn field="phone"></AgGridColumn>
+              <AgGridColumn field="email"></AgGridColumn>
+          </AgGridReact>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/Employees.js
@@ -0,0 +1,124 @@
+const Employees = [
+  {
+    name: "John Smith",
+    title: "Chief Executive Official",
+    department: "Executive",
+    salary: 1000000,
+    phone: "720-555-5551",
+    email: "jsmith@company.com"
+  },
+  {
+    name: "Jane Doe",
+    title: "Chief Technology Official",
+    department: "Executive",
+    salary: 1000000,
+    phone: "720-555-5552",
+    email: "jdoe@company.com"
+  },
+  {
+    name: "George Washington",
+    title: "HR Manager",
+    department: "Human Resources",
+    salary: 500000,
+    phone: "720-555-5553",
+    email: "gwashington@company.com"
+  },
+  {
+    name: "Abraham Lincoln",
+    title: "General Manager",
+    department: "Management",
+    salary: 500000,
+    phone: "720-555-5554",
+    email: "alincoln@company.com"
+  },
+  {
+    name: "Bill Gates",
+    title: "Software Engineer",
+    department: "IT",
+    salary: 100000,
+    phone: "720-555-5555",
+    email: "bgates@company.com"
+  },
+  {
+    name: "Steve Jobs",
+    title: "Software Engineer",
+    department: "IT",
+    salary: 100000,
+    phone: "720-555-5556",
+    email: "sjobs@company.com"
+  },
+  {
+    name: "Donald Trump",
+    title: "Sales Represenative",
+    department: "Sales",
+    salary: 60000,
+    phone: "720-555-5557",
+    email: "dtrump@company.com"
+  },
+  {
+    name: "Joe Biden",
+    title: "Sales Represenative",
+    department: "Sales",
+    salary: 60000,
+    phone: "720-555-5558",
+    email: "jbiden@company.com"
+  },
+  {
+    name: "Benjamin Franklin",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5559",
+    email: "bfranklin@company.com"
+  },
+  {
+    name: "Alexander Hamilton",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-55510",
+    email: "ahamilton@company.com"
+  },
+  {
+    name: "Samuel Adams",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5511",
+    email: "sadams@company.com"
+  },
+  {
+    name: "John Adams",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5512",
+    email: "jadams@company.com"
+  },
+  {
+    name: "James Madison",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5513",
+    email: "jmadison@company.com"
+  },
+  {
+    name: "John Jay",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5514",
+    email: "jjay@company.com"
+  },
+  {
+    name: "Thomas Jefferson",
+    title: "Production Associate",
+    department: "Production",
+    salary: 40000,
+    phone: "720-555-5515",
+    email: "tjefferson@company.com"
+  }
+]
+
+export default Employees;
